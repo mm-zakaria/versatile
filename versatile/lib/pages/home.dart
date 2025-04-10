703c0cab9d609abc45e103e1fccdb20d3897f79d
mm-zakaria <mmzakarianwucse103@gmail.com>
2025-04-10 23:20:01 +0600

    work on my application

--- versatile/lib/pages/home.dart
+++ versatile/lib/pages/home.dart
@@ -20,235 +20,237 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xfff2f2f2),
-      body: Container(
-        margin: EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Hey, Zakaria",
-                      style: Appwidget.boldText(),
-                    ),
-                    Text(
-                      "Good Afternoon",
-                      style: Appwidget.lightText(),
-                    ),
-                  ],
-                ),
-                Image.asset(
-                  "images/boy.jpg",
-                  height: 90,
-                  width: 80,
-                  fit: BoxFit.cover,
-                )
-              ],
-            ),
-            SizedBox(height: 30.0),
-            Container(
-              padding: EdgeInsets.only(left: 20.0),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
-              width: MediaQuery.of(context).size.width,
-              child: TextField(
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Search here",
-                    hintStyle: Appwidget.lightText(),
-                    suffixIcon: Icon(
-                      Icons.search,
-                      color: Colors.black,
-                    )),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Hey, Zakaria",
+                        style: Appwidget.boldText(),
+                      ),
+                      Text(
+                        "Good Afternoon",
+                        style: Appwidget.lightText(),
+                      ),
+                    ],
+                  ),
+                  Image.asset(
+                    "images/boy.jpg",
+                    height: 90,
+                    width: 80,
+                    fit: BoxFit.cover,
+                  )
+                ],
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Categories",
-                    style: TextStyle(
+              SizedBox(height: 30.0),
+              Container(
+                padding: EdgeInsets.only(left: 20.0),
+                decoration: BoxDecoration(
+                    color: Colors.white, borderRadius: BorderRadius.circular(10)),
+                width: MediaQuery.of(context).size.width,
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search here",
+                      hintStyle: Appwidget.lightText(),
+                      suffixIcon: Icon(
+                        Icons.search,
                         color: Colors.black,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold)),
-                Text("See more",
-                    style: TextStyle(
-                        color: Colors.amber,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold)),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: [
-                Container(
-                  height: 180,
-                  padding: EdgeInsets.all(20.0),
-                  margin: EdgeInsets.only(right: 20.0),
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Center(
-                      child: Text(
-                    "All ",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold),
-                  )),
+                      )),
                 ),
-                Expanded(
-                  child: Container(
-                    height: 180,
-                    child: ListView.builder(
-                        padding: EdgeInsets.zero,
-                        itemCount: categories.length,
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) {
-                          return CategoriesTile(image: categories[index]);
-                        }),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("All Products",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold)),
-                Text("See more",
-                    style: TextStyle(
-                        color: Colors.amber,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold)),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Container(
-              height: 200,
-              child: ListView(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Categories",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold)),
+                  Text("See more",
+                      style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold)),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Row(
                 children: [
                   Container(
+                    height: 180,
+                    padding: EdgeInsets.all(20.0),
                     margin: EdgeInsets.only(right: 20.0),
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    decoration: BoxDecoration(color: Colors.white , borderRadius: BorderRadius.circular(15.0)),
-                    child: Column(
-                      children: [
-                        Image.asset("images/laptop2.png",
-                            height: 130, width: 130, fit: BoxFit.cover),
-                        Text(
-                          "Laptop",
-                          style: Appwidget.boldText(),
-                        ),
-                        Row(
-                          children: [
-                            Text("\$100",
-                                style: TextStyle(
-                                    color: Colors.amber,
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.bold)),
-                            SizedBox(
-                              width: 40.0,
-                            ),
-                            Container(
-                                decoration: BoxDecoration(color: Colors.amber),
-                                child: Icon(
-                                  Icons.add,
-                                  color: Colors.white,
-                                ),),
-                          ],
-                        ),
-                      ],
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(15.0),
                     ),
+                    child: Center(
+                        child: Text(
+                      "All ",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
+                    )),
                   ),
-
-
-
-                   Container(
-                    margin: EdgeInsets.only(right: 20.0),
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    decoration: BoxDecoration(color: Colors.white , borderRadius: BorderRadius.circular(15.0)),
-                    child: Column(
-                      children: [
-                        Image.asset("images/watch2.png",
-                            height: 130, width: 130, fit: BoxFit.cover),
-                        Text(
-                          "Smart Watch",
-                          style: Appwidget.boldText(),
-                        ),
-                        Row(
-                          children: [
-                            Text("\$250",
-                                style: TextStyle(
-                                    color: Colors.amber,
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.bold)),
-                            SizedBox(
-                              width: 40.0,
-                            ),
-                            Container(
-                                decoration: BoxDecoration(color: Colors.amber),
-                                child: Icon(
-                                  Icons.add,
-                                  color: Colors.white,
-                                ),),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Container(
-                    margin: EdgeInsets.only(right: 20.0),
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    decoration: BoxDecoration(color: Colors.white , borderRadius: BorderRadius.circular(15.0)),
-                    child: Column(
-                      children: [
-                        Image.asset("images/headphone2.png",
-                            height: 130, width: 130, fit: BoxFit.cover),
-                        Text(
-                          "Wired Headphone",
-                          style: Appwidget.boldText(),
-                        ),
-                        Row(
-                          children: [
-                            Text("\$99",
-                                style: TextStyle(
-                                    color: Colors.amber,
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.bold)),
-                            SizedBox(
-                              width: 40.0,
-                            ),
-                            Container(
-                                decoration: BoxDecoration(color: Colors.amber),
-                                child: Icon(
-                                  Icons.add,
-                                  color: Colors.white,
-                                ),),
-                          ],
-                        ),
-                      ],
+                  Expanded(
+                    child: Container(
+                      height: 180,
+                      child: ListView.builder(
+                          padding: EdgeInsets.zero,
+                          itemCount: categories.length,
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return CategoriesTile(image: categories[index]);
+                          }),
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("All Products",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold)),
+                  Text("See more",
+                      style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold)),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                height: 200,
+                child: ListView(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 20.0),
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      decoration: BoxDecoration(color: Colors.white , borderRadius: BorderRadius.circular(15.0)),
+                      child: Column(
+                        children: [
+                          Image.asset("images/laptop2.png",
+                              height: 130, width: 130, fit: BoxFit.cover),
+                          Text(
+                            "Laptop",
+                            style: Appwidget.boldText(),
+                          ),
+                          Row(
+                            children: [
+                              Text("\$100",
+                                  style: TextStyle(
+                                      color: Colors.amber,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold)),
+                              SizedBox(
+                                width: 40.0,
+                              ),
+                              Container(
+                                  decoration: BoxDecoration(color: Colors.amber),
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                  ),),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+        
+        
+        
+                     Container(
+                      margin: EdgeInsets.only(right: 20.0),
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      decoration: BoxDecoration(color: Colors.white , borderRadius: BorderRadius.circular(15.0)),
+                      child: Column(
+                        children: [
+                          Image.asset("images/watch2.png",
+                              height: 130, width: 130, fit: BoxFit.cover),
+                          Text(
+                            "Smart Watch",
+                            style: Appwidget.boldText(),
+                          ),
+                          Row(
+                            children: [
+                              Text("\$250",
+                                  style: TextStyle(
+                                      color: Colors.amber,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold)),
+                              SizedBox(
+                                width: 40.0,
+                              ),
+                              Container(
+                                  decoration: BoxDecoration(color: Colors.amber),
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                  ),),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+        
+                    Container(
+                      margin: EdgeInsets.only(right: 20.0),
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      decoration: BoxDecoration(color: Colors.white , borderRadius: BorderRadius.circular(15.0)),
+                      child: Column(
+                        children: [
+                          Image.asset("images/headphone2.png",
+                              height: 130, width: 130, fit: BoxFit.cover),
+                          Text(
+                            "Wired Headphone",
+                            style: Appwidget.boldText(),
+                          ),
+                          Row(
+                            children: [
+                              Text("\$99",
+                                  style: TextStyle(
+                                      color: Colors.amber,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold)),
+                              SizedBox(
+                                width: 40.0,
+                              ),
+                              Container(
+                                  decoration: BoxDecoration(color: Colors.amber),
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                  ),),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
